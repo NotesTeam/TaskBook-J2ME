d894e6da335707f619974dc1560b4ac9d7c3f85e
Wojciech Zieba <wojciech.zieba@protonmail.com>
2017-03-08 15:45:06 +0100

    Extracting List to another class

--- src/MainMidlet.java
+++ src/MainMidlet.java
@@ -1,35 +1,21 @@
-import java.util.Enumeration;
-import java.util.Vector;
-
-import javax.microedition.lcdui.Choice;
-import javax.microedition.lcdui.Command;
-import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
-import javax.microedition.lcdui.Displayable;
-import javax.microedition.lcdui.Form;
-import javax.microedition.lcdui.List;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
-public class MainMidlet extends MIDlet implements CommandListener, ListUpdateListener {
+public class MainMidlet extends MIDlet {
 
 	public static Display display;
-	private Command addNote;
-	private Command selectNote;
-	private Form addNoteForm;
-	private List list;
+	private NotesList notesList;
 
 	public MainMidlet() {
 		display = Display.getDisplay(this);
-		list = new List("TaskBook", Choice.IMPLICIT);
-		addNoteForm = new AddNoteForm(this, list);
-		setupCommands();
+		notesList = new NotesList();
 	}
 
 	protected void startApp() throws MIDletStateChangeException {
 		System.out.println("Midlet: startApp()");
-		display.setCurrent(list);
-		inflateList();
+		display.setCurrent(notesList);
+		notesList.inflateList();
 	}
 
 	protected void pauseApp() {
@@ -39,58 +25,8 @@ public class MainMidlet extends MIDlet implements CommandListener, ListUpdateLis
 	protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
 
 	}
-
-	public void commandAction(Command c, Displayable d) {
-		if (c == addNote) 
-			addNewNote();
-		else if (c == selectNote)
-			showNoteDetails();
-	}
 	
 	public static Display getDisplay() {
 		return display;
-	}
-	
-	public void onListUpdate() {
-		notifyItemInserted();
-	}
-	
-	private void setupCommands() {
-		addNote = new Command("Add", Command.SCREEN, 0);
-		selectNote = new Command("Select", Command.SCREEN, 1);		
-		list.addCommand(addNote);
-		list.addCommand(selectNote);
-		list.setCommandListener(this);
-	}
-
-	private void showNoteDetails() {
-		Vector vector = RecordStoreManager.getInstance().getRecords();
-		Note note = (Note) vector.elementAt(list.getSelectedIndex());
-		System.out.println("Command clicked: Select note: " + note.toString());
-		display.setCurrent(new ShowNoteForm(list, note));
-	}
-
-	private void inflateList() {
-		Vector vector = RecordStoreManager.getInstance().getRecords();
-		Enumeration enumeration = vector.elements();
-		while(enumeration.hasMoreElements())
-			addListRow(enumeration);
-	}
-
-	private void addListRow(Enumeration enumeration) {
-		Note note = (Note) enumeration.nextElement();
-		System.out.println("Read: " + note.toString());
-		list.append(note.getTitle(), null);
-	}
-
-	private void notifyItemInserted() {
-		Vector vector = RecordStoreManager.getInstance().getRecords();
-		Note note = (Note) vector.lastElement();
-		list.append(note.getTitle(), null);
-	}
-
-	private void addNewNote() {
-		System.out.println("Command clicked: Add new note");
-		display.setCurrent(addNoteForm);
 	}
 }
---
+++ src/NotesList.java
@@ -0,0 +1,76 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.List;
+
+public class NotesList extends List implements CommandListener, ListUpdateListener {
+	
+	private Command addNote;
+	private Command selectNote;
+	private Display display;
+	private Form addNoteForm;
+
+	public NotesList() {
+		super("TaskBook", Choice.IMPLICIT);
+		display = MainMidlet.getDisplay();
+		addNoteForm = new AddNoteForm(this, this);
+		setupCommands();
+	}
+	
+	private void setupCommands() {
+		addNote = new Command("Add", Command.SCREEN, 0);
+		selectNote = new Command("Select", Command.SCREEN, 1);		
+		addCommand(addNote);
+		addCommand(selectNote);
+		setCommandListener(this);
+	}
+
+	public void commandAction(Command c, Displayable d) {
+		if (c == addNote) 
+			addNewNote();
+		else if (c == selectNote)
+			showNoteDetails();
+	}
+	
+	public void inflateList() {
+		Vector vector = RecordStoreManager.getInstance().getRecords();
+		Enumeration enumeration = vector.elements();
+		while(enumeration.hasMoreElements())
+			addListRow(enumeration);
+	}
+	
+	public void onListUpdate() {
+		notifyItemInserted();
+	}
+
+	private void addListRow(Enumeration enumeration) {
+		Note note = (Note) enumeration.nextElement();
+		System.out.println("Read: " + note.toString());
+		this.append(note.getTitle(), null);
+	}
+
+	private void notifyItemInserted() {
+		Vector vector = RecordStoreManager.getInstance().getRecords();
+		Note note = (Note) vector.lastElement();
+		this.append(note.getTitle(), null);
+	}
+	
+	private void addNewNote() {
+		System.out.println("Command clicked: Add new note");
+		display.setCurrent(addNoteForm);
+	}
+	
+	private void showNoteDetails() {
+		Vector vector = RecordStoreManager.getInstance().getRecords();
+		Note note = (Note) vector.elementAt(getSelectedIndex());
+		System.out.println("Command clicked: Select note: " + note.toString());
+		display.setCurrent(new ShowNoteForm(this, note));
+	}
+
+}
