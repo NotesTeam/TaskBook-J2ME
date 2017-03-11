496ec82bd8e039603137c6c23d80c095b814371d
Wojciech Zieba <wojciech.zieba@protonmail.com>
2017-03-11 13:24:09 +0100

    JSON implemantation

--- src/ByteUtils.java
+++ src/ByteUtils.java
@@ -4,40 +4,82 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import org.json.me.JSONException;
+import org.json.me.JSONObject;
+
+
 public class ByteUtils {
 	
 	public static byte[] toByteArray(Note note) {
 		ByteArrayOutputStream bout = new ByteArrayOutputStream();
 		DataOutputStream dout = new DataOutputStream(bout);
-		try {
-			dout.writeUTF(note.getTitle());
-			dout.writeUTF(note.getContent());
-			dout.writeLong(note.getTimestamp());
-			dout.writeUTF(note.getCategory().toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		
+		JSONObject jsonNote = parseToJson(note);
+		
+		writeToStream(dout, jsonNote);
 		return bout.toByteArray();
 	}
 
 	public static Note toNote(byte[] bytes) {
 		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
 		DataInputStream dis = new DataInputStream(bais);
-		String title = "";
-		String content = "";
-		long timestamp = 0;
-		String sCategory = "";
+		
+		String noteFromStream = readFromStream(dis);
+		JSONObject jsonNote = readJson(noteFromStream);
+		Note note = parseJsonToNote(jsonNote);
+		
+		return note;
+	}
+
+	private static Note parseJsonToNote(JSONObject jsonNote) {
+		Note note = new Note(
+				jsonNote.optString(Note.TITLE),
+				jsonNote.optString(Note.CONTENT),
+				jsonNote.optLong(Note.TIMESTAMP),
+				Category.toCategory(jsonNote.optString(Note.CATEGORY))
+				);
+		return note;
+	}
+
+	private static JSONObject readJson(String noteFromStream) {
+		JSONObject jsonNote = null;
+		
 		try {
-			title = dis.readUTF();
-			content = dis.readUTF();
-			timestamp = dis.readLong();
-			sCategory = dis.readUTF();
+			jsonNote = new JSONObject(noteFromStream);
+					} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		return jsonNote;
+	}
+
+	private static String readFromStream(DataInputStream dis) {
+		String noteFromStream = null;
+		try {
+			noteFromStream = dis.readUTF();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		Category category = Category.toCategory(sCategory);
-		return new Note(title, content, timestamp, category);   
+		return noteFromStream;
+	}
+	
+	private static void writeToStream(DataOutputStream dout, JSONObject jsonNote) {
+		try {
+			dout.writeUTF(jsonNote.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static JSONObject parseToJson(Note note) {
+		JSONObject jsonNote = new JSONObject();
+		try {
+			jsonNote.put(Note.TITLE, note.getTitle());
+			jsonNote.put(Note.CONTENT, note.getContent());
+			jsonNote.put(Note.TIMESTAMP, note.getTimestamp());
+			jsonNote.put(Note.CATEGORY, note.getCategory());
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		return jsonNote;
 	}
 }
